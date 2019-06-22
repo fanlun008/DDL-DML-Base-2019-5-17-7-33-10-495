f07a194386e8f953fc2fea34036fddf75b9d00f6
fanlun008 <51396450+fanlun008@users.noreply.github.com>
2019-06-22 23:55:13 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+insert into student values(003, '王五',18,'男');
 -- 修改记录  
-
+update student set name = '赵六' where id = '001';
 --  删除记录  
-
+delete from student where id = '001';
 -- 查询记录  
+select * from student;
